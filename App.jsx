32b1312ec2d23e0504c0ebc1d296881c2c6364fd
ShigatsuEl <shigatsu970704@gmail.com>
2021-10-15 21:20:23 +0900

    02. TextInput

--- App.jsx
+++ App.jsx
@@ -1,26 +1,54 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState } from "react";
 import {
   StyleSheet,
   Text,
-  TouchableHighlight,
+  TextInput,
   TouchableOpacity,
   View,
 } from "react-native";
 import { theme } from "./color";
 
 export default function App() {
+  const [mode, setMode] = useState("work");
+  const [text, setText] = useState("");
+  const setModeTravel = () => setMode("travel");
+  const setModeWork = () => setMode("work");
+  const onChangeText = (payload) => setText(payload);
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.header}>
-        <TouchableOpacity>
-          <Text style={styles.btnText}>Work</Text>
+        <TouchableOpacity onPress={setModeWork}>
+          <Text
+            style={{
+              ...styles.btnText,
+              color: mode === "work" ? "white" : theme.gray,
+            }}
+          >
+            Work
+          </Text>
         </TouchableOpacity>
-        <TouchableOpacity>
-          <Text style={styles.btnText}>Travel</Text>
+        <TouchableOpacity onPress={setModeTravel}>
+          <Text
+            style={{
+              ...styles.btnText,
+              color: mode === "travel" ? "white" : theme.gray,
+            }}
+          >
+            Travel
+          </Text>
         </TouchableOpacity>
       </View>
+      <TextInput
+        onChangeText={onChangeText}
+        value={text}
+        placeholder={
+          mode === "work" ? "Add a To Do" : "Where do you want to go?"
+        }
+        style={styles.input}
+      />
     </View>
   );
 }
@@ -39,6 +67,13 @@ const styles = StyleSheet.create({
   btnText: {
     fontSize: 38,
     fontWeight: "600",
-    color: theme.gray,
+  },
+  input: {
+    backgroundColor: "white",
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginTop: 20,
+    fontSize: 18,
   },
 });
